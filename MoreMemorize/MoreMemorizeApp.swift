4414c82243ac55c9d5ba4fa27b9bf0ff657624a5
Sebastian Pfeufer <sebastianpfeufer@icloud.com>
2022-04-07 14:43:46 +0200

    Initial Commit

---
+++ MoreMemorize/MoreMemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoreMemorizeApp.swift
+//  MoreMemorize
+//
+//  Created by Sebastian Pfeufer on 07.04.22.
+//
+
+import SwiftUI
+
+@main
+struct MoreMemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
