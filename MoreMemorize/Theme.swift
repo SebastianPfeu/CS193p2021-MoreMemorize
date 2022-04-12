37b3328b6fa949e66695a2f254f782d4882a3747
Sebastian Pfeufer <sebastianpfeufer@icloud.com>
2022-04-12 15:09:45 +0200

    Added concept of theme to model

---
+++ MoreMemorize/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  MoreMemorize
+//
+//  Created by Sebastian Pfeufer on 12.04.22.
+//
+
+import Foundation
+
+struct Theme {
+    var name: String
+    var emojis: Array<String>
+    var numberOfPairsOfCards: Int
+    var color: String
+}
